a38b484378064d1abc690ec74e81d7e4b560a464
alinyyess <alina.aman.09@mail.ru>
2026-05-21 18:24:19 +0500

    added new homework peremennye and td

---
+++ peremennye and td.js
@@ -0,0 +1,56 @@
+const name='Alina'
+console.log(name)
+
+let age=15 
+console.log(age)
+
+age = 16
+console.log(age)
+
+var surname ='Amangeldy'
+console.log(surname)
+
+var surname ='Salimbayeva'
+console.log(surname)
+
+let integer=20
+console.log(integer)
+
+let sentence="Hello, World!"
+console.log(sentence)
+
+let isTrue=true
+console.log(isTrue)
+
+let isFalse=false
+console.log(isFalse)
+
+let nothing=null
+console.log(nothing)
+
+let undefinedValue 
+console.log(undefinedValue)
+
+let notANumber = NaN
+console.log(notANumber)
+
+let num=100
+let strNum =String(num)
+console.log(strNum)
+
+let line="200"
+let numLine= Number(line)
+console.log(numLine)
+
+let strUndefined= String(undefinedValue)
+console.log(strUndefined)
+
+let numNothing= Number(nothing)
+console.log(numNothing)
+
+let num2=300
+let strNum2= String(num2)
+console.log(strNum2)
+
+let input = prompt('What is your name?')
+let input2 = parseInt(prompt('How old are you?'))
