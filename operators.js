a51a526630b4a07c501b32f8b3bd64d3bc00e16e
alinyyess <alina.aman.09@mail.ru>
2026-05-28 19:33:24 +0500

    added new hw operators

---
+++ operators.js
@@ -0,0 +1,42 @@
+let a=10
+let b=5
+let c=2
+let sum=a+b+c
+console.log(sum)
+let diff=b-(a+c)
+console.log(diff)
+let protient=a*b/c
+console.log(protient)
+let remainder=a%b
+console.log(remainder)
+
+let x=7
+let incr=++x + ++x
+let decr=--x - --x
+console.log(decr)
+console.log(incr)
+
+let y=4
+let z=5
+let sum1=y+z
+let line="The result is:"
+console.log(sum1)
+console.log(line+sum1)
+
+console.log(a>b)
+console.log(a>0)
+console.log(b>0)
+console.log(c<10 || c==2)
+console.log(a/c != 5)
+
+let num=10
+let str= '10'
+console.log(num==str)
+console.log(num===str) 
+
+let everyIsTrue = ((x%2==0 || x%3==0) && x%6!=0)
+console.log(everyIsTrue)
+
+let TrueE = !(y>10 || y<5)
+console.log(TrueE)
+
