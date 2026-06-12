1bd1a5360a20d2bc011eb12c7339dcafe965ae7c
alinyyess <alina.aman.09@mail.ru>
2026-06-12 17:54:22 +0500

    added new hw objects

--- objects.js
+++ objects.js
@@ -7,11 +7,61 @@ console.log(person);
 
 console.log(person.name)
 console.log(person.age)
+person.city = "Astana"
 
 
+person.greet = function(){
+    console.log("Hi, my name is " + this.name + ", I'm " + this.age + " years old!")
+}
+person.greet()
 
-let person ={
-    name : "Alina",
-    age : 16,
-    city : "Almaty"
+
+let obj1 ={
+    a: 10,
+    b:20
 };
+
+let obj2 ={
+    a: 10,
+    b: 20
+};
+
+console.log(obj1==obj2)
+console.log(obj1===obj2)
+
+
+let book ={
+    title : "The Picture of Dorian Grey",
+    author : "Oscar Wilde",
+    details : {
+        year : 1890,
+        pages : 436
+    }
+}
+Object.assign(book)
+book.details.year = 1891
+console.log(book)
+
+
+let calculator ={
+    a: 5,
+    b: 6 
+}
+
+calculator.sum = function(){
+    console.log(this.a + this.b)
+}
+calculator.multiply = function(){
+    console.log(this.a * this.b)
+}
+calculator.sum()
+calculator.multiply()
+
+
+const car ={
+    brand : "Dior",
+    model : "Adriana Lima"
+}
+
+car.brand = "Gucci"
+console.log(car)
